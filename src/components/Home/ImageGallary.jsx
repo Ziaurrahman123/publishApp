86275d944470f2a3dd69337e7e4feaff4be83909
Ziaurrahman123 <89965618+Ziaurrahman123@users.noreply.github.com>
2021-09-24 12:45:22 +0530

    comment

--- src/components/Home/ImageGallary.jsx
+++ src/components/Home/ImageGallary.jsx
@@ -5,28 +5,28 @@ export const ImageGallary = () => {
     <table>
       <tr>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Shah_Rukh_Khan" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg/330px-Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg" title="Shahrukh Khan" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Salman_Khan" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Salman_Khan_at_Renault_Star_Guild_Awards.jpg/220px-Salman_Khan_at_Renault_Star_Guild_Awards.jpg" title="Salman Khan" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Madhuri_Dixit" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Madhuri_Dixit_promoting_Total_Dhamaal_in_2019_%28cropped%29.jpg/220px-Madhuri_Dixit_promoting_Total_Dhamaal_in_2019_%28cropped%29.jpg" title="Madhuri Dixit" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Nora_Fatehi" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Nora_Fatehi_snapped_at_Krome_studio_in_Bandra.jpg/220px-Nora_Fatehi_snapped_at_Krome_studio_in_Bandra.jpg" title="Nora Fatehi" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Aamir_Khan" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Aamir_Khan_graces_the_first_look_launch_of_%E2%80%98Secret_Superstar%E2%80%99.jpg/220px-Aamir_Khan_graces_the_first_look_launch_of_%E2%80%98Secret_Superstar%E2%80%99.jpg" title="Aamir Khan" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Katrina_Kaif" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Katrina_Kaif_promoting_Bharat_in_2019.jpg/220px-Katrina_Kaif_promoting_Bharat_in_2019.jpg" title="Katrina Kaif" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Akshay_Kumar" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Akshay_Kumar.jpg/220px-Akshay_Kumar.jpg" title="Akshay Kumar" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Deepika_Padukone" imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Deepika_Padukone_Cannes_2018_%28cropped%29.jpg/220px-Deepika_Padukone_Cannes_2018_%28cropped%29.jpg" title="Deepika Padukone" />
         </td>
       </tr>
     </table>
